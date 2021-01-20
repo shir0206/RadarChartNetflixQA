bf9440c6d8ff4d909122f98941c5b1c11c29ac9d
Shir Zabolotny <40990488+shir0206@users.noreply.github.com>
2021-01-20 22:40:49 +0200

    Add 2 Charts & questions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,211 @@
 import React, { useEffect, useRef, useState } from "react";
 import Chartjs from "chart.js";
 import "./styles.css";
+import { Bar, Radar } from "react-chartjs-2";
+
+const chartDataBar = {
+  labels: [
+    "TV Action & Adventure",
+    "TV Comedies",
+    "TV Dramas",
+    "CrimeTV Shows",
+    "Kids'TV",
+    "RomanticTV Shows"
+  ],
+  datasets: [
+    {
+      label: "# of TV-Shows in Brazil",
+      data: [0, 5, 8, 4, 1, 1],
+
+      backgroundColor: [
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)"
+      ],
+      borderColor: [
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235,1)",
+        "rgba(54, 162, 235, 1)"
+      ],
+
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in China",
+      data: [6, 8, 17, 4, 2, 17],
+
+      backgroundColor: [
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)"
+      ],
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in Russia",
+      data: [1, 2, 6, 6, 4, 1],
+
+      backgroundColor: [
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)"
+      ],
+      borderColor: [
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)"
+      ],
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in Turkey",
+      data: [5, 2, 21, 10, 0, 5],
+
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)"
+      ],
+      borderWidth: 1
+    }
+  ]
+};
+
+const chartDataRadar = {
+  labels: [
+    "TV Action & Adventure",
+    "TV Comedies",
+    "TV Dramas",
+    "CrimeTV Shows",
+    "Kids'TV",
+    "RomanticTV Shows"
+  ],
+  datasets: [
+    {
+      label: "# of TV-Shows in Israel",
+      data: [3, 1, 5, 3, 0, 0],
+
+      backgroundColor: [
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(54, 162, 235, 0.2)"
+      ],
+      borderColor: [
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(54, 162, 235,1)",
+        "rgba(54, 162, 235, 1)"
+      ],
+
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in Japan",
+      data: [3, 6, 19, 3, 13, 17],
+
+      backgroundColor: [
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)",
+        "rgba(255, 128, 0, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)",
+        "rgba(255, 128, 0, 1)"
+      ],
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in United Kingdom",
+      data: [2, 35, 22, 42, 12, 9],
+
+      backgroundColor: [
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(75, 192, 192, 0.2)"
+      ],
+      borderColor: [
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(75, 192, 192, 1)"
+      ],
+      borderWidth: 1
+    },
+    {
+      label: "# of TV-Shows in Spain",
+      data: [2, 5, 7, 17, 3, 8],
+
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 99, 132, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(255, 99, 132, 1)"
+      ],
+      borderWidth: 1
+    }
+  ]
+};
 
 const chartConfig = {
   type: "radar",
@@ -137,12 +342,18 @@ const Chart = () => {
     }
   }, [chartContainer]);
 
-  const updateDataset = (datasetIndex, newData) => {
-    chartInstance.data.datasets[datasetIndex].data = newData;
-    chartInstance.update();
-  };
+  // const updateDataset = (datasetIndex, newData) => {
+  //   chartInstance.data.datasets[datasetIndex].data = newData;
+  //   chartInstance.update();
+  // };
 
-  const questions = [
+  const [started, setStarted] = useState(false);
+  const [ms, setMs] = useState(0);
+  const [report, setReport] = useState(false);
+  const [radar, setRadar] = useState(false);
+  const [bar, setBar] = useState(false);
+
+  const questionsRadar = [
     "דוגמא: כמה סדרות ילדים יש באנגליה?",
     "כמה סדרות מסוג דרמה יש ביפן?",
     "כמה סדרות מסוג אקשיין, רומנטיקה וקומדיה יש בישראל?",
@@ -155,9 +366,18 @@ const Chart = () => {
     "הסוף! תודה על ההשתתפות :)"
   ];
 
-  const [started, setStarted] = useState(false);
-  const [ms, setMs] = useState(0);
-  const [report, setReport] = useState(false);
+  const questionsBar = [
+    "דוגמא: כמה סדרות ילדים יש ברוסיה?",
+    "כמה סדרות מסוג דרמה יש בסין?",
+    "כמה סדרות מסוג אקשיין, רומנטיקה וקומדיה יש ברוסיה?",
+    "לאיזו מדינה יש הכי הרבה סדרות מסוג פשע?",
+    "לאיזו 2 מדינות יש אותה כמות של סדרות באותה הקטגוריה?",
+    "לאיזו מדינות אין סדרות מכל 4 הקטגוריות?",
+    "לאיזו מדינה יש 5 סדרות מסוג אקשיין?",
+    "לאיזו מדינה יש הכי הרבה סדרות?",
+    "באיזו קטגוריה יש הכי הרבה סדרות לברזיל?",
+    "הסוף! תודה על ההשתתפות :)"
+  ];
 
   const intervalRef = useRef();
 
@@ -181,8 +401,78 @@ const Chart = () => {
   return (
     <div>
       <h1 className="x">Number of TV-Shows in Netflix</h1>
-      <canvas ref={chartContainer} />
 
+      <button
+        onClick={() => {
+          setRadar(false);
+          setBar(true);
+          setIndex(0);
+          setAllAnswer([]);
+          setReport(false);
+        }}
+      >
+        Bar
+      </button>
+
+      <button
+        onClick={() => {
+          setRadar(true);
+          setBar(false);
+          setIndex(0);
+          setAllAnswer([]);
+          setReport(false);
+        }}
+      >
+        Radar
+      </button>
+      {bar && (
+        <Bar
+          data={chartDataBar}
+          options={{
+            title: {
+              display: true,
+              text: "Bar Chart",
+              fontSize: 25
+            },
+            legend: {
+              display: true,
+              position: "bottom",
+              labels: {
+                fontColor: "#000"
+              }
+            }
+          }}
+        />
+      )}
+
+      {radar && (
+        <Radar
+          data={chartDataRadar}
+          options={{
+            title: {
+              display: true,
+              text: "Radar Chart",
+              fontSize: 25
+            },
+            legend: {
+              display: true,
+              position: "bottom",
+              labels: {
+                fontColor: "#000"
+              }
+            },
+            scale: {
+              angleLines: {
+                display: false
+              },
+              ticks: {
+                suggestedMin: -10,
+                suggestedMax: 45
+              }
+            }
+          }}
+        />
+      )}
       <hr></hr>
 
       <button
@@ -195,9 +485,16 @@ const Chart = () => {
 
       {started && (
         <div className="qstn">
-          <h4>
-            {index + 1}. {questions[index]}
-          </h4>
+          {radar && (
+            <h4>
+              {index + 1}. {questionsRadar[index]}
+            </h4>
+          )}
+          {bar && (
+            <h4>
+              {index + 1}. {questionsBar[index]}
+            </h4>
+          )}
           <input
             onChange={(e) => {
               setAnswer(e.target.value);
@@ -234,9 +531,14 @@ const Chart = () => {
         >
           Report
         </button>
-        <h4>{ms}</h4>
+        <h6>{ms}</h6>
 
-        {report && <ol>{listItems}</ol>}
+        {report && (
+          <>
+            <h4>{radar ? "Radar Answers" : "Bar Answers"}</h4>
+            <ol>{listItems}</ol>
+          </>
+        )}
       </div>
     </div>
   );
